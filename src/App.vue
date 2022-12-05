4b4d35975e3aeec57609ef73d00954fbb35948d3
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-05 18:19:04 +0100

    import Montserrat font

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
 </style>
