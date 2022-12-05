cccc86bd7b4a368fc5d821b3b3d7b5ef69765fdf
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-05 18:27:56 +0100

    creazione macro-componenti

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    
+    <HeaderPage></HeaderPage>
+    <MainPage></MainPage>
+    <FooterPage></FooterPage>
   </div>
 </template>
 
 <script>
+import HeaderPage from '@/components/HeaderPage.vue';
+import MainPage from '@/components/MainPage.vue';
+import FooterPage from '@/components/FooterPage.vue';
+
 export default {
+  components: {
+    HeaderPage,
+    MainPage,
+    FooterPage
+  }
 
 }
 </script>
