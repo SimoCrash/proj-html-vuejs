6705e009a34e79f31f324b361bda3997a52894c8
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-05 18:08:45 +0100

    import fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'; // libreria icone del progetto
+import { faChevronRight, faHatWizard } from '@fortawesome/free-solid-svg-icons'; // importiamo le icone che ci servono
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; // importazione del componente FontAwesomeIcon
+
+library.add(faChevronRight, faHatWizard); // aggiungiamo le icone nella libreria del progetto
+Vue.component('FontAwesomeIcon', FontAwesomeIcon); // registrazione globale del componente FontAwesomeIcon per renderlo usabile in qualsiasi parte del codice senza ulteriore importazione
 
 Vue.config.productionTip = false
 
