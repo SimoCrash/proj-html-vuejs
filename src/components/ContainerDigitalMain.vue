92897386606ad17c76b7fef0e47c81473b3720e8
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-06 17:36:08 +0100

    push per ticket

--- src/components/ContainerDigitalMain.vue
+++ src/components/ContainerDigitalMain.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    
+  <div class="container-page">
+    <h2 class="secondary-title">Let's Dream Big Together</h2>
+    <h1 class="primary-title">
+      We are pioneers of the digital approach, using leading-edge
+      technology to simplify procedures and aplly executive
+      coaching in the new age of digitalization.
+    </h1>
+    <div class="container-cards">
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <div>
+      <button></button>
+      <button></button> 
+    </div>
   </div>
 </template>
 
@@ -10,6 +45,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .container-page {
+    max-width: 55%;
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .secondary-title {
+      color: #45B9A7;
+      text-align: center;
+    }
+    .primary-title {
+      text-align: center;
+    }
+  }
 </style>
