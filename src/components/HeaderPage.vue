05d41474abf1751a1a5e903847c91b88cc422912
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-06 02:03:04 +0100

    importate tutte le icone da utilizzare

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    
+    <font-awesome-icon icon="fa-solid fa-chevron-down" />
+    <font-awesome-icon icon="fa-regular fa-circle-user" />
+    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+    <font-awesome-icon icon="fa-solid fa-ruler" /> <!-- icona a pagamento -->
+    <font-awesome-icon icon="fa-solid fa-circle-radiation" /> <!-- icona a pagamento-->
+    <font-awesome-icon icon="fa-solid fa-book" />
+    <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+    <font-awesome-icon icon="fa-solid fa-circle-chevron-up" /> <!-- icona a pagamento-->
+    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" /> <!-- icona a pagamento-->
+    <font-awesome-icon icon="fa-solid fa-location-dot" /> <!-- icona a pagamento-->
+    <font-awesome-icon icon="fa-solid fa-arrow-right-long" /> 
+    <font-awesome-icon icon="fa-regular fa-circle" />
+    <font-awesome-icon icon="fa-solid fa-spinner" />
+    <font-awesome-icon icon="fa-solid fa-people-arrows" />
+    <font-awesome-icon icon="fa-solid fa-map-location-dot" />
+    <font-awesome-icon icon="fa-regular fa-clock" />
+    <font-awesome-icon icon="fa-solid fa-globe" />
+    <font-awesome-icon icon="fa-solid fa-person" />
+    <font-awesome-icon icon="fa-solid fa-code" />
+    <h1>Hello World</h1>
   </div>
 </template>
 
@@ -10,6 +29,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    div {
+        height: 100vh;
+        background-color: green;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,49 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'; // libreria icone del progetto
-import { faChevronRight, faHatWizard } from '@fortawesome/free-solid-svg-icons'; // importiamo le icone che ci servono
+import { faChevronDown, 
+         faMagnifyingGlass, 
+         faRuler, 
+         faCircleRadiation, 
+         faBook,
+         faCartShopping,
+         faCircleChevronUp,
+         faCircleChevronDown,
+         faLocationDot,
+         faArrowRightLong,
+         faSpinner,
+         faPeopleArrows,
+         faMapLocationDot,
+         faGlobe,
+         faPerson,
+         faCode
+        } from '@fortawesome/free-solid-svg-icons'; // importiamo le icone che ci servono
+import {faCircleUser,
+        faCircle,
+        faClock,
+        } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; // importazione del componente FontAwesomeIcon
 
-library.add(faChevronRight, faHatWizard); // aggiungiamo le icone nella libreria del progetto
+library.add(faChevronDown,
+            faCircleUser, 
+            faMagnifyingGlass,
+            faRuler,
+            faCircleRadiation,
+            faBook,
+            faCartShopping,
+            faCircleChevronUp,
+            faCircleChevronDown,
+            faLocationDot,
+            faArrowRightLong,
+            faCircle,
+            faSpinner,
+            faPeopleArrows,
+            faMapLocationDot,
+            faClock,
+            faGlobe,
+            faPerson,
+            faCode
+            ); // aggiungiamo le icone nella libreria del progetto
 Vue.component('FontAwesomeIcon', FontAwesomeIcon); // registrazione globale del componente FontAwesomeIcon per renderlo usabile in qualsiasi parte del codice senza ulteriore importazione
 
 Vue.config.productionTip = false
