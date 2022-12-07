a345af9ece1efd586101f4da600b10241f0e9e14
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-07 05:36:23 +0100

    fine giornata

--- src/components/ContainerFaqMain.vue
+++ src/components/ContainerFaqMain.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    
+  <div class="container-page">
+    <div class="container-bg-top">
+      <img src="@/img/background-map-dots-top-right-transparent.png" alt="">
+    </div>
+    <div class="container-bg-bottom">
+      <img src="@/img/maxcoach-shape-07-100x100.png" alt="">
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,20 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .container-page {
+    background-color: #323043;
+  }
+  .container-bg-top {
+    display: flex;
+    justify-content: flex-end;
+  }
+  .container-bg-bottom {
+    position: relative;
+    img {
+      position: absolute;
+      left: 100px;
+      bottom: 0px;
+    }
+  }
 </style>
