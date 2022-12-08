aea196fefe6c4711c95432668354370254e7ea5a
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-08 18:55:58 +0100

    Main completo senza effetti

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container-bg">
     
   </div>
 </template>
@@ -10,6 +10,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .container-bg {
+    background-color: #171621;
+    color: #7E7E7E;
+    position: relative;
+  }
 </style>
